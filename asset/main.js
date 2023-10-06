241d964d8aefe50b5e57f4c3b87db0d4f46f8dea
Yuji070902 <93475805+Yuji-Terada@users.noreply.github.com>
2023-10-06 23:25:34 +0900

    Add files via upload

--- asset/main.js
+++ asset/main.js
@@ -2,24 +2,3 @@ $(window).on('load',function(){
     $('#quote-container').addClass('appear');
     $('header').addClass('appear');
 });
-
-/*
-function PageTopAnime() {
-    var scroll = $(window).scrollTop();
-
-    if(scroll >= 1500){   //上から400pxスクロールしたら
-        $("#quote-container").addClass("scrolled");
-    }
-    else{
-        $("#quote-container").removeClass("scrolled");
-    }
-}
-
-$(window).scroll(function (){
-    PageTopAnime();
-});
-
-$(window).on('load', function() {
-    PageTopAnime();
-})
-*/
